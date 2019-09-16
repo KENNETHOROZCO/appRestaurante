622edeceb40395cc1a1fdb33c6e2cf905126a70b
Kenneth Orozco <kntor497@gmail.com>
2019-09-15 21:58:07 -0600

    Pantalla Restaurante
    Pantalla de registro de restaurantes

--- app/src/main/java/kenneth/orozco/apprestaurante/MainActivity.java
+++ app/src/main/java/kenneth/orozco/apprestaurante/MainActivity.java
@@ -2,7 +2,9 @@ package kenneth.orozco.apprestaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void cambiar(View view){
+        Intent intent = new Intent(this,PantallaAgregarRest.class);
+        startActivity(intent);
+    }
+
 }
